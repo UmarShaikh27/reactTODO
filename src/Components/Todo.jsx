fec5892a57d8513de22482717c5d45f49bb366a7
Umar Habib <you@example.comumarhabib464@gmail.com>
2022-02-13 16:16:53 +0500

    Todo react complete

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -5,6 +5,8 @@ function Todo() {
 
     const [todo,setTodo] = useState([])
     const [inp,inpvalue] = useState()
+    const [myIndex,setMyIndex] = useState()
+    const [editInputValue,setEditInputValue] = useState()
 
     
 
@@ -12,30 +14,73 @@ function Todo() {
        
         todo.push(inp)
         setTodo([...todo])
-    
-       
-     
-    
-        // {console.log(inp.length);}
     }
+
+
     const removeFunc = () => {
         setTodo([])
     }
 
+    const editTodo = (ind) => {
+  
+      setMyIndex(ind)
+    }
+
+    const deleteTodo = (ind) => {
+      console.log(ind);
+      todo.splice(ind,1)
+      setTodo([...todo])
+    }
+
+    const updateValue = () => {
+      console.log("oyi");
+      todo.splice(myIndex, 1, editInputValue);
+      setTodo([...todo]);
+      setMyIndex("");
+      setEditInputValue("");
+    }
+
+
   return (
-    <div className='container'>
-      Hello world
+    <div className='container box'>
+     <h1 >Enter Todo</h1>
       <input
        type="text"
-       className='input-group'
+       className='input-group form-control'
        onChange={(e) => {inpvalue(e.target.value)}}
         />
-        <button className='btn btn-primary' onClick={addFunc}>Add Todo</button>
-        <button className='btn btn-danger' onClick={removeFunc}>Remove Todo</button>
+      <div className="btnDiv">
+      <button className='btn btn-primary  mx-3 my-3' onClick={addFunc}>Add Todo</button>
+        <button className='btn btn-danger mx-3 my-3' onClick={removeFunc}>Remove Todo</button>
+      </div>
 
-        <div>
+        <div className='my-4'>
         {todo.map((value, index, array) => {
-          return <li className='theList' key={index}> {value} </li>;
+          return index === myIndex ? (
+            <div className='d-flex'>
+              <input
+                key={index}
+                placeholder="Edit value"
+                value={editInputValue}
+                className="input-group-sm form-control"
+                onChange={(e) => setEditInputValue(e.target.value)}
+              />
+              <button className="btn btn-info" onClick={updateValue}>
+                update
+              </button>
+          
+            </div>
+           
+          ):(
+            <div className='contentArea my-1'>
+                <li className='theList' key={index}> {value} </li>
+                <div>
+                   <button className="btn btn-sm btn-success mx-2" onClick={() => editTodo(index)}>Edit</button>
+                   <button className="btn btn-sm btn-danger" onClick={() => deleteTodo(index)}>Delete</button>
+                </div>
+                
+            </div>
+          )
         })}
         </div>
     </div>
